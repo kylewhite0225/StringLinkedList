c711fc25c64070f1cc09bb1e174a565f85b6a8e5
zli-BC <71357470+zli-BC@users.noreply.github.com>
2020-10-26 18:06:29 -0700

    Initial commit

---
+++ ass4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "SLLString.h"
+
+using namespace std;
+
+int main(){
+    SLLString str("Hello world!");
+    SLLString newStr = new SLLString;
+
+    newStr = str;
+    newStr += SLLString(" CS@BC");
+    newStr[6] = 'W';
+    
+    cout << newStr << endl; // Hello World! CS@BC
+    cout << newStr.length() << endl; //18
+    
+    int loc = newStr.findSubstring("World");
+    cout << loc << endl; // 6
+    
+    newStr.erase('l'); //erase the letter l.
+    cout << newStr << endl; // Heo Word! CS@BC
+    
+    newStr.erase('C');
+    cout << newStr << endl; // Heo Word! S@B
+    
+    return 0;
+}
