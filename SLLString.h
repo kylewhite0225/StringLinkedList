bea317b39b610d670f01576f50ded2fa90fd78fd
Kyle White <kyle.white@bellevuecollege.edu>
2020-11-04 20:48:40 -0800

    Formatting

--- SLLString.h
+++ SLLString.h
@@ -19,32 +19,35 @@ private:
     Node *head = NULL;
     Node *tail = NULL;
     int size = 0;
+
 public:
     // Default constructor
     SLLString();
     // Copy constructor accepting a string as a parameter
-    SLLString(const string& other);
+    SLLString(const string &other);
     // Destructor
     ~SLLString();
     // Copy constructor accepting an SLLString object as a parameter
     SLLString(const SLLString &other);
     // Assignment constructor
-    SLLString& operator=(const SLLString& other);
+    SLLString &operator=(const SLLString &other);
     // Overloaded concatenation operator
-    SLLString& operator+=(const SLLString& other);
+    SLLString &operator+=(const SLLString &other);
     // Length function which returns the length (size) of the SLLString object
-    int length();   
+    int length();
     // Returns the character stored in the list at index n
-    char& operator[](const int n);
+    char &operator[](const int n);
     // Finds the index of the first occurence of substring, returns -1 if not found.
-    int findSubstring(const SLLString& substring);
+    int findSubstring(const SLLString &substring);
     // Helper function for findSubstring which accepts a string and creates an SLLString object.
-    int findSubstring(const string & substring);
+    int findSubstring(const string &substring);
     // Erases all occurrences of character c from an SLLString object..
     void erase(char c);
     // Assigning the << operator as a friend allows iostream to print SLLString objects.
-    friend ostream& operator<<(ostream& out, const SLLString & s) {
-        for(Node* n = s.head; n != NULL; n = n->next) {
+    friend ostream &operator<<(ostream &out, const SLLString &s)
+    {
+        for (Node *n = s.head; n != NULL; n = n->next)
+        {
             out << n->data;
         }
         return out;
